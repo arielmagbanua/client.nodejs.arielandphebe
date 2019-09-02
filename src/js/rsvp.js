4d18a7d53a0b7280547ad196d02732e50cca4941
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2019-09-02 22:43:36 +0800

    Added modal notification after submission of RSVP.

--- src/js/rsvp.js
+++ src/js/rsvp.js
@@ -1,11 +1,14 @@
 (() => {
+    const nameElement = document.querySelector('form#rsvp-form #name');
+    const emailElement = document.querySelector('form#rsvp-form #email');
+    const attendanceElement = document.querySelector('input[name="attendance"]:checked');
+    const noteElement = document.querySelector('form#rsvp-form #note');
+    const rsvpSubmitButton = document.getElementById('rsvp-submit');
+
     document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('form#rsvp-form').addEventListener('submit', (event) => {
             event.preventDefault();
-            const nameElement = document.querySelector('form#rsvp-form #name');
-            const emailElement = document.querySelector('form#rsvp-form #email');
-            const attendanceElement = document.querySelector('input[name="attendance"]:checked');
-            const noteElement = document.querySelector('form#rsvp-form #note');
+            rsvpSubmitButton.value = 'Submitting...';
 
             fetch('/api/send-rsvp', {
                 method: 'POST',
@@ -17,14 +20,28 @@
                     note: noteElement.value
                 })
             }).then((response) => response.json())
-                .then((response) => {
-                    console.log(response);
-                    // TODO: notify user via modal that rsvp was sent successfully.
+                .then((responseData) => {
+                    console.log(responseData);
+                    document.querySelector('p#rsvp-modal-message').innerHTML = responseData.message;
+                    $('#rsvp-confirmation-modal').modal('show');
+                    rsvpSubmitButton.value = 'Send RSVP';
+                    clearFields();
                 })
                 .catch((error) => {
                     console.log(error)
                     // TODO: notify user via modal that rsvp was sent unsuccessfully.
+                    document.querySelector('p#rsvp-modal-message').innerHTML = 'Sorry RSVP was not sent. Please try again later.';
+                    $('#rsvp-confirmation-modal').modal('show');
+                    rsvpSubmitButton.value = 'Send RSVP';
+                    clearFields();
                 });
         });
     });
+
+    function clearFields() {
+        nameElement.value = '';
+        emailElement.value = '';
+        noteElement.value = '';
+        document.getElementById('radio-yes').checked = true;
+    }
 })();
